af35a9d620bd9b76ac09b97afedf5f10e9ab127a
nandana-n29 <nandananair2906@gmail.com>
2024-05-25 21:52:11 +0530

    Fees Management

---
+++ src/feesmanagement/jTable1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package feesmanagement;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Nandana Nair
+ */
+class jTable1 {
+
+    static void setModel(DefaultTableModel model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
